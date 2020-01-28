f96ef09e74bd779ae614507b6cc18f79b8e46897
kavinrajasekaran <43220430+kavinrajasekaran@users.noreply.github.com>
2020-01-27 17:15:12 -0800

    Shows what spots are free and blocked on the maze.
    Shows empty spots as "0" and filled spots as "1".

---
+++ MazeSolver.java
@@ -0,0 +1,71 @@
+ 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MazeSolver {
+
+	public static void main(String[] args) {
+		
+		
+		 try {
+             
+	          //BufferedReader in = new BufferedReader(new FileReader("/Users/rajr/Desktop/eclipse-workspace/Playground/src/test/input.txt"));
+			 BufferedReader in = new BufferedReader(new FileReader("maze2.txt"));
+	           
+			 String line;
+			 int lineNumber = 0;
+			 int mazeRows = 0;
+			 int mazeColumns = 0;
+			 int[][] maze = null;
+			 
+			 //initialize the 2D maze
+	          while ((line = in.readLine()) != null) {
+	        	  if(lineNumber == 0) {
+	        		  mazeRows = Integer.parseInt(line);
+	        	  }
+	        	  else if(lineNumber == 1) {
+	        		  mazeColumns = Integer.parseInt(line);
+	        		  maze= new int[mazeRows][mazeColumns];
+	        	  } else {
+	        		  String[] rowsCols = line.split("\\s+");
+	        		  int rowNum = Integer.parseInt(rowsCols[0]);
+	        		  int colNum = Integer.parseInt(rowsCols[1]);
+	        		  maze[rowNum][colNum] = 1;
+	        		  
+	        	  }
+	        	  lineNumber++;
+	        	  //System.out.println(line);
+	          }
+	          
+	         printMaze(maze);
+	         
+	         
+	         
+	         in.close();
+		 }  catch (IOException e) {
+	            System.out.println("I/O Error: " + e.getMessage());
+	        }
+		 
+
+	}
+	
+	public static void printMaze(int[][] table) {
+		int rows = table.length;
+		int cols = table[0].length;
+		for(int i= 0; i < rows; i++) {
+			for(int j=0; j < cols; j++) {
+				//System.out.println("table[" + i + "]["+ j +"]: " + table[i][j]);
+				System.out.print(table[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println(" ");
+		}
+		
+		
+	}
+	
+	
+
+}
